ae8f6bd2028fbd7cb0b711a0617ad9826d435c1e
racsogl <oscar.gl@mundo-r.com>
2023-03-27 20:36:43 +0200

    rutas

--- RoutesProject/src/app/app-routing/app-routing.module.ts
+++ RoutesProject/src/app/app-routing/app-routing.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from '../error/error.component';
+import { HelloComponent } from '../hello/hello.component';
+import { HomeComponent } from '../home/home.component';
 
-
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // cuando el path es vacio que se vaya a home
+  { path: 'home', component: HomeComponent }, // cuando en el path aparezca home
+  { path: 'hello/:name', component: HelloComponent }, // solo se puede ir al componente Hello si le paso un nombre en la url hello/foo
+  { path: 'homes', redirectTo:'/home', pathMatch: 'full'},
+  { path: '**', component: ErrorComponent } // cuando entremos en una página que no exista en ninguna ruta
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
