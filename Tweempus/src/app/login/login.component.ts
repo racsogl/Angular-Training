d6bcf9b433ed3677c6f4fe4ad645c70dce780e64
racsogl <oscar.gl@mundo-r.com>
2023-04-09 19:25:06 +0200

    login

--- Tweempus/src/app/login/login.component.ts
+++ Tweempus/src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, FormControl, Validators} from "@angular/forms";
+
+import {AuthenticationService} from "../core/authentication.service";
+import {AuthorService} from "../shared/author/author.service";
 
 @Component({
   selector: 'tweempus-login',
@@ -7,10 +11,31 @@ import {Component, OnInit} from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() {
+  // formulario
+  userForm!: FormGroup
+  userNoExist: boolean = false
+
+  constructor(
+    private authService: AuthenticationService,
+    private authorService: AuthorService,
+    private formBuilder: FormBuilder
+  ) {
   }
 
   ngOnInit() {
+    this.userForm = this.formBuilder.group({
+      idAuthor: ['', Validators.required]
+    })
+  }
+
+  login(userForm: FormGroup) {
+    if (this.userNoExist) {
+      this.userNoExist = false
+    }
+    this.authorService.getAuthor(this.userForm?.value.idAuthor).subscribe(
+      author => this.authService.login(this.userForm?.value.idAuthor),
+      error => this.userNoExist = true
+    )
   }
 
 }
--- Tweempus/src/app/login/login.module.ts
+++ Tweempus/src/app/login/login.module.ts
@@ -4,11 +4,13 @@ import {CommonModule} from '@angular/common';
 import {LoginRoutingModule} from './login-routing.module';
 
 import {LoginComponent} from './login.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    ReactiveFormsModule
   ],
   declarations: [LoginComponent]
 })
